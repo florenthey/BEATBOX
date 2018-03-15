ee7d7b3b458e0208c948fed69e59ba4405a821b8
Flo <florentheyraud.pro@gmail.com>
2018-03-15 14:47:16 +0100

    ca va marcher

---
+++ rythme.js
@@ -0,0 +1,63 @@
+
+function play1() {
+
+  var sound = document.getElementById("boom");
+    sound.play();
+ }
+
+
+function play2() {
+
+  var sound = document.getElementById("clap");
+    sound.play();
+  }
+
+
+  function play3() {
+
+  var sound = document.getElementById("hihat");
+    sound.play();
+  }
+
+
+  function play4() {
+
+  var sound = document.getElementById("kick");
+    sound.play();
+  }
+
+
+  function play5() {
+
+  var sound = document.getElementById("openhat");
+    sound.play();
+  }
+
+
+  function play6() {
+
+  var sound = document.getElementById("ride");
+    sound.play();
+  }
+
+
+
+  function play7() {
+
+  var sound = document.getElementById("snare");
+    sound.play();
+  }
+
+
+  function play8() {
+
+  var sound = document.getElementById("tink");
+    sound.play();
+  }
+
+
+  function play9() {
+
+  var sound = document.getElementById("tom");
+    sound.play();
+  }
